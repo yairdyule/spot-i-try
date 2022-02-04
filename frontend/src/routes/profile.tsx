3471153550c1d212fd53e1c3046babf64e7a9bd5
Jared Jewell <jaje9434@colorado.edu>
2022-02-04 08:23:51 -0700

    add context

--- frontend/src/routes/profile.tsx
+++ frontend/src/routes/profile.tsx
@@ -1,7 +1,26 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../hooks/UserContext";
+import Main from "../components/Main";
+
 export default function Profile() {
+  const User = useContext(UserContext);
+
+  const isLoggedIn = User?.user?.loggedIn;
+  const name = User?.user?.name;
+
   return (
-    <main className="flex flex-col">
-      <div>WIP - report back soon!</div>
-    </main>
+    <Main>
+      {isLoggedIn ? (
+        <h1>Hello, {name}!</h1>
+      ) : (
+        <h1>
+          You must{" "}
+          <Link to="/login" className="text-emerald-300 font-semibold">
+            login
+          </Link>
+        </h1>
+      )}
+    </Main>
   );
 }
