70c12583754f49e977c9fbac68b4dc7b6f0ca232
Jared Jewell <jaje9434@colorado.edu>
2022-02-05 09:34:11 -0700

    shrink icons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import { MdPersonOutline } from "react-icons/md";
 function App() {
   return (
     <div className="App">
-      <nav className="flex flex-row gap-4 justify-center py-4 border-b-2 border-emerald-300 text-2xl bg-slate-emerald-800">
+      <nav className="flex flex-row gap-4 justify-center py-4 border-b-2 border-emerald-300 text-xl bg-slate-emerald-800">
         <Navlink path="/authorize" Child={AiOutlineLock} />
         <Navlink path="/" Child={AiOutlineHome} />
         <Navlink path="/search" Child={AiOutlineSearch} />
