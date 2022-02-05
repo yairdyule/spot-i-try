69bf1934e1703abc61b7ba15733ba79d987e2d30
Jared Jewell <jaje9434@colorado.edu>
2022-02-04 22:46:49 -0700

    fetch details, conditionally render 'em

--- frontend/src/components/ProfileDetails.tsx
+++ frontend/src/components/ProfileDetails.tsx
@@ -1,25 +1,72 @@
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../hooks/UserContext";
 
 type Props = {
   id: number;
 };
 
-type ProfileDetails = {};
+type ProfileDetails = {
+  friends: [{ name: string }];
+  incomingQueues: [{ title: string }];
+  sentQueues: [{ title: string }];
+  profile: {
+    bio: string;
+  };
+} | null;
+
+enum Classnames {
+  col = "flex flex-col border-2 border-emerald-300",
+  row = "flex flex-row justify-center space-between gap-6",
+}
 
 export default function ProfileDetails({ id }: Props) {
-  const [data, setData] = useState("");
+  const [details, setDetails] = useState(null as ProfileDetails);
+  const User = useContext(UserContext);
+
   useEffect(() => {
     axios
       .post("http://localhost:8000/user/details", { id: id })
-      .then(({ data }) => {
-        console.log(data);
-        setData("score");
+      .then((data) => {
+        console.log(data.data.details);
+        setDetails(data.data.details);
       });
     return () => {
-      setData("");
+      setDetails(null);
     };
   }, []);
 
-  return <h1>{data}</h1>;
+  return (
+    <>
+      {details && <h1>{details.profile.bio}</h1>}
+      <div className={Classnames.row}>
+        <div className={Classnames.col}>
+          <h1 className="border-2 border-b-slate-200">Friends </h1>
+          <ul>
+            {details?.friends.map((friend: { name: string }) => {
+              return <li>{friend.name}</li>;
+            })}
+          </ul>
+          {/*add friend icon*/}
+        </div>
+        <div className={Classnames.col}>
+          <h1 className="border-b-slate-200">Incoming Queues</h1>
+          <ul>
+            {details?.incomingQueues.map((queue: { title: string }) => {
+              return <li>{queue.title}</li>;
+            })}
+          </ul>
+        </div>
+        <div className={Classnames.col}>
+          <h1 className="border-b-slate-200">Outgoing Queues</h1>
+          <ul>
+            {details?.sentQueues.map((queue: { title: string }) => {
+              return <li>{queue.title}</li>;
+            })}
+          </ul>
+          {/*add  queue icon*/}
+        </div>
+      </div>
+    </>
+  );
 }
