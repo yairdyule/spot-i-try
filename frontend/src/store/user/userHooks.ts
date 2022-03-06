99c618e16cd0af581c47d256d6a7b2af0af75c08
Jared Jewell <jaje9434@colorado.edu>
2022-03-05 21:37:05 -0700

    create user store, hooks

---
+++ frontend/src/store/user/userHooks.ts
@@ -0,0 +1,25 @@
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { User } from "../../types";
+import { AppDispatch, login, logout, RootState } from "./userStore";
+
+/**
+ * selectors
+ */
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useUserSelector = () => useAppSelector((state) => state.User);
+
+/**
+ * dispatches
+ */
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+
+export const useLoginDispatch = (User: User) => {
+  const dispatch = useAppDispatch();
+  dispatch(login(User));
+};
+
+export const useLogoutDispatch = () => {
+  const dispatch = useAppDispatch();
+  dispatch(logout());
+};
